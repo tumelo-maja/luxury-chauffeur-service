83a62e6d4a699c42f3bf04f00d8031010a6f2cd6
tumelo-maja <mogale.ramsey@gmail.com>
2025-06-21 20:33:57 +0100

    add file and configs to prepare for Heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn chauffeur.wsgi
--- chauffeur/settings.py
+++ chauffeur/settings.py
@@ -30,10 +30,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['127.0.0.1',
+                 '.herokuapp.com',]
 
 # Application definition
 
