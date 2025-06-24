4045de3a4f427a29198fd2209dddafade26e3d3f
tumelo-maja <mogale.ramsey@gmail.com>
2025-06-24 12:49:39 +0100

    Add login link and style front end form

--- chauffeur/settings.py
+++ chauffeur/settings.py
@@ -140,3 +140,9 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+ACCOUNT_LOGIN_METHODS = {'email'}
+ACCOUNT_SIGNUP_FIELDS = ['email*', 'username*', 'password1*', 'password2*']
