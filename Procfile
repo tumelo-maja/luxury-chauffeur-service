d0a9ab7dfc894f2084d2affc5fdcac82ac44c616
tumelo-maja <mogale.ramsey@gmail.com>
2025-06-25 17:30:18 +0100

    rename 'chauffeur' project core folder to 'core'

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn chauffeur.wsgi
+web: gunicorn core.wsgi
--- core/wsgi.py
+++ core/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for chauffeur project.
+WSGI config for chauffeur services project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chauffeur.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
