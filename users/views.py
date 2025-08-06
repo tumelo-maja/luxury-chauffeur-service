3c5212196385f81bf62e41438ec3126d6c3abd71
tumelo-maja <mogale.ramsey@gmail.com>
2025-08-06 19:07:10 +0100

    adjust and style the box layout for floating containers

--- users/views.py
+++ users/views.py
@@ -32,7 +32,7 @@ def profile_edit_view(request):
         form = PassengerEditForm
     else:
         role = ManagerProfile.objects.filter(profile=profile_user).first()
-        form = []
+        form = ManagerEditForm
 
 
     if request.method == 'POST':
