587278d2e84a6efc7fc1919be52fdd5fec7565f4
tumelo-maja <mogale.ramsey@gmail.com>
2025-08-19 20:08:18 +0100

    addd docstrign for apps.py for trips

--- trips/apps.py
+++ trips/apps.py
@@ -2,5 +2,16 @@ from django.apps import AppConfig
 
 
 class TripsConfig(AppConfig):
+    """
+    Configuration for the `trips` application.
+
+    Attributes
+    ----------
+    default_auto_field : str
+        The type of primary key field to use for models by default.
+    name : str
+        name of the application.
+    """        
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'trips'
+
