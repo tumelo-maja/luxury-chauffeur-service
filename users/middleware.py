9ed50d3392e85f0e61943ff69e7cb768ae71a281
tumelo-maja <mogale.ramsey@gmail.com>
2025-08-19 10:03:50 +0100

    add docstrings to users middleware.py

--- users/middleware.py
+++ users/middleware.py
@@ -1,11 +1,39 @@
+"""
+Custom middleware for the `users` app.
+"""
 
 
 class StoreUserRoleMiddleware:
-    # store variable 'rolel' in sessions
+    """
+    Middleware to store the user's role in the session when signing up.
+
+    Attributes
+    ----------
+    get_response : callable
+        The next view in within the request session.
+
+    Methods
+    -------
+    __call__(request)
+        Stores the 'role' argument, passed with the request, in the session.
+    """
+
+
     def __init__(self, get_response):
         self.get_response = get_response
 
+
     def __call__(self, request):
+        """
+        Process the request before passing it further.
+
+        Gets the 'role' query parameter and stored in the session.
+
+        Returns
+        -------
+        HttpResponse
+            The response object passed to the next view with `role` in its session.        
+        """        
         role = request.GET.get('role')
         if role in ['driver', 'passenger']:
             request.session['role'] = role
