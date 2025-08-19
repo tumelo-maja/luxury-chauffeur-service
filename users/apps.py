bb7306864e3ec02c59d771f1455c84f0427add6d
tumelo-maja <mogale.ramsey@gmail.com>
2025-08-19 09:55:32 +0100

    add docstrings to app.py

--- users/apps.py
+++ users/apps.py
@@ -2,8 +2,21 @@ from django.apps import AppConfig
 
 
 class UsersConfig(AppConfig):
+    """
+    Configuration for the `users` application.
+
+    Attributes
+    ----------
+    default_auto_field : str
+        The type of primary key field to use for models by default.
+    name : str
+        name of the application.
+    """    
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
 
     def ready(self):
+        """
+        Triggered when Django starts to defined and register the signal handlers in `users.signals`.
+        """        
         import users.signals
