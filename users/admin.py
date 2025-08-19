36f6ac9ffe30e7848e048100becc40d9550b5ccf
tumelo-maja <mogale.ramsey@gmail.com>
2025-08-19 10:43:43 +0100

    remove comment

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import Profile, DriverProfile, PassengerProfile, ManagerProfile
 
-# Register your models here.
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'user_type', 'phone')
