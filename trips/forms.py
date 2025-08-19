3a8811e2211536e0beb66d301b6bf2e631411df8
tumelo-maja <mogale.ramsey@gmail.com>
2025-08-19 19:06:08 +0100

    add docstring for the Trip model

--- trips/forms.py
+++ trips/forms.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 from datetime import timedelta
 from .models import *
 
-RATING_LABELS = [('', '<-Select a rating->')]+RATING_OPTIONS
+RATING_LABELS = [('', '<-Select a rating->')]+Trip.RATING_OPTIONS
 
 
 class TripRequestForm(ModelForm):
--- trips/models.py
+++ trips/models.py
@@ -2,44 +2,49 @@ from django.db import models
 import shortuuid
 from users.models import DriverProfile, PassengerProfile
 
-# Create your models here.
-
-# CONSTANTS
-VEHICLE_CHOICES = [
-    ("Rolls Royce Phantom", "Rolls Royce Phantom"),
-    ("Range Rover Vogue", "Range Rover Vogue"),
-    ("Mercedes Benz V-Class", "Mercedes Benz V-Class"),
-    ("Premium Limousine", "Premium Limousine"),
-    ("Classic Vintage Cars", "Classic Vintage Cars"),
-]
-
-TRIP_TYPES = [
-    ("Airport Transfers", "Airport Transfers"),
-    ("Special Events", "Special Events"),
-    ("Corporate Chauffeur", "Corporate Chauffeur"),
-    ("Private & VIP Chauffeur", "Private & VIP Chauffeur"),
-]
-
-STATUS_OPTIONS = [
-    ("pending", "Pending"),
-    ("confirmed", "Confirmed"),
-    ("in_progress", "In progress"),
-    ("cancelled", "Cancelled"),
-    ("modified", "Modified"),
-    ("completed", "Completed"),
-    ("rejected", "Rejected"),
-]
-
-RATING_OPTIONS = [
-    (1, "1 Star"),
-    (2, "2 Stars"),
-    (3, "3 Stars"),
-    (4, "4 Stars"),
-    (5, "5 Stars"),
-]
-
 
 class Trip(models.Model):
+    """
+    Handles trip instance creation.
+
+    Stores details about the trip instance including passenger, driver, travel_date etc., methods and other properties.
+
+    """
+
+    VEHICLE_CHOICES = [
+        ("Rolls Royce Phantom", "Rolls Royce Phantom"),
+        ("Range Rover Vogue", "Range Rover Vogue"),
+        ("Mercedes Benz V-Class", "Mercedes Benz V-Class"),
+        ("Premium Limousine", "Premium Limousine"),
+        ("Classic Vintage Cars", "Classic Vintage Cars"),
+    ]
+
+    TRIP_TYPES = [
+        ("Airport Transfers", "Airport Transfers"),
+        ("Special Events", "Special Events"),
+        ("Corporate Chauffeur", "Corporate Chauffeur"),
+        ("Private & VIP Chauffeur", "Private & VIP Chauffeur"),
+    ]
+
+    STATUS_OPTIONS = [
+        ("pending", "Pending"),
+        ("confirmed", "Confirmed"),
+        ("in_progress", "In progress"),
+        ("cancelled", "Cancelled"),
+        ("modified", "Modified"),
+        ("completed", "Completed"),
+        ("rejected", "Rejected"),
+    ]
+
+    RATING_OPTIONS = [
+        (1, "1 Star"),
+        (2, "2 Stars"),
+        (3, "3 Stars"),
+        (4, "4 Stars"),
+        (5, "5 Stars"),
+    ]
+
+
     trip_name = models.CharField(
         max_length=128, unique=True, default=shortuuid.uuid)
     passenger = models.ForeignKey(
@@ -49,20 +54,16 @@ class Trip(models.Model):
     travel_datetime = models.DateTimeField(null=True, blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
-
     trip_type = models.CharField(max_length=50, choices=TRIP_TYPES)
     comments = models.TextField(blank=True, null=True)
     driver = models.ForeignKey(
         DriverProfile, on_delete=models.CASCADE, related_name='trips_driver')
-
     status = models.CharField(
         max_length=50, choices=STATUS_OPTIONS, default="pending")
-
     vehicle = models.CharField(max_length=100, choices=VEHICLE_CHOICES)
 
     passenger_rating = models.IntegerField(choices=RATING_OPTIONS, null=True, blank=True)
     driver_rating = models.IntegerField(choices=RATING_OPTIONS, null=True, blank=True)
-
     passenger_rating_comments = models.TextField(blank=True, null=True)
     driver_rating_comments = models.TextField(blank=True, null=True)
 
@@ -79,10 +80,6 @@ class Trip(models.Model):
     @property
     def status_str(self):
         return self.status.replace('_', ' ').capitalize()
-
-    @property
-    def location_end_short(self):
-        return self.location_end.split(',')[0]
     
     def start_trip(self):
         self.driver.update_status('engaged')
