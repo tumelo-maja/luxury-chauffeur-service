f9abe1c7e2cec66df83135b7f847eddf386ba336
tumelo-maja <mogale.ramsey@gmail.com>
2025-08-20 09:49:48 +0100

    add TestContactFormView to home/tests

--- home/tests/test_views.py
+++ home/tests/test_views.py
@@ -1,7 +1,8 @@
 from django.test import TestCase, SimpleTestCase
 from django.shortcuts import reverse
+from ..forms import UserContactForm
 
-class TestHomeViews(SimpleTestCase):
+class TestHomeView(SimpleTestCase):
 
     def test_home_page_uses_correct_template(self):
         response = self.client.get('/')
@@ -21,4 +22,15 @@ class TestHomeViews(SimpleTestCase):
         self.assertContains(response, f'<a href="{reverse("account_login")}">Login</a>')
         self.assertContains(response, f'<a href="{reverse("signup_type")}">Signup</a>')
 
-               
+
+class TestContactFormView(SimpleTestCase):
+
+    def test_contact_page_uses_correct_template(self):
+        response = self.client.get(reverse("contact"))
+        self.assertTemplateUsed(response,'home/contact.html')
+
+    def test_render_enquiry_form_in_contact_page(self):
+        response = self.client.get(reverse("contact"))
+        self.assertContains(response,'Send Us Your Enquiry', status_code=200)
+        self.assertContains( response, "Receive a copy of this enquiry")
+        self.assertIsInstance(response.context['form'], UserContactForm)
